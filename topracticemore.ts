41f79f01690a1e2161e11408395d546bc2a7ff13
Felipe <machadofelipe2016@outlook.com>
2023-03-08 12:46:34 -0300

    c0- to practice a pure typescript component

---
+++ topracticemore.ts
@@ -0,0 +1,15 @@
+
+
+const a:number = 2323
+const b:number =5000
+
+const sum = a+b
+console.log(sum)
+
+
+const filterName = (item:string) => {
+    return item === 'felipe'
+}
+const Name:string[] = ['felipe', 'martins','felipe'].filter(filterName)
+
+console.log(Name)
