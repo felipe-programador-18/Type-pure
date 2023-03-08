74f955864552cbcd4d9ead355afaecb996b23c78
Felipe <machadofelipe2016@outlook.com>
2023-03-08 12:49:13 -0300

    to generate another index structure when I building my indexts

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var axios_1 = require("axios");
+var URL = "https://jsonplaceholder.typicode.com/todos/1";
+axios_1["default"].get(URL)
+    .then(function (res) {
+    console.log(res.data);
+});
